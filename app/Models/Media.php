cb54d8ebfd9774e8c51995efbbc9f6dc093b3850
n_ducquy <n_ducquy@thk-hd.vn>
2026-03-09 16:51:20 +0700

    hot fix

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -1,31 +1,11 @@
 <?php
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Awcodes\Curator\Models\Media as CuratorMedia;
 
-class Media extends Model
+class Media extends CuratorMedia
 {
-    protected $fillable = [
-        'page_type',
-        'name',
-        'file_path',
-        'file_type',
-        'mime_type',
-        'file_size',
-        'description',
-        'alt_text',
-        'width',
-        'height',
-    ];
-
-    protected function casts(): array
-    {
-        return [];
-    }
-
-    public function getUrlAttribute(): string
-    {
-        return $this->file_path ? asset('storage/'.$this->file_path) : '';
-    }
+    use HasUuids;
+    
 }
