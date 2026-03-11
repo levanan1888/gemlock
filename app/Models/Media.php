7b83377440358a89b014a9016833b6df272260d1
n_ducquy <n_ducquy@thk-hd.vn>
2026-03-11 08:39:27 +0700

    hot fix

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -2,10 +2,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Awcodes\Curator\Models\Media as CuratorMedia;
 
 class Media extends CuratorMedia
 {
     use HasUuids;
-    
+    public function yes(): BelongsTo
+    {
+        return $this->belongsTo(yes::class);
+    }
 }
